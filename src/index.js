99d5d3e8ac9698cfced62c937c1f94b3ff9041c1
Pavel Vasilev <pavelabadon@gmail.com>
2025-09-21 09:41:14 +0300

    Add resources

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('A dano raboti')
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('raboti be');
+});
+
+app.listen(5000, () => console.log('Express Server is listening on http://localhost:5000...'))
