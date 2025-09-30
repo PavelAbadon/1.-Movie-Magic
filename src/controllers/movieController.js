fb1e722e89ee255ea1739df31e924cc3c72c4f46
Pavel Vasilev <pavelabadon@gmail.com>
2025-09-30 16:39:33 +0300

    Show static search page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -23,6 +23,10 @@ movieController.get('/:movieId/details',(req, res) =>{
     res.render('details', { movie });
 } )
 
+movieController.get('/search', (req, res) =>{
+    res.render("search")
+})
+
 
 
 export default movieController;
