763de7c700c940de066f6f52a49a73dce391d5aa
Pavel Vasilev <pavelabadon@gmail.com>
2025-10-12 11:32:17 +0300

    Edit movies functionality

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -63,11 +63,28 @@ movieController.get('/:movieId/delete', isAuth, async (req, res) =>{
     if (!movie.creator?.equals(req.user.id)) {
         return res.redirect('/');
     }
-    
+
     await movieService.delete(movieId);
 
     res.redirect('/');
 })
+
+movieController.get('/:movieId/edit', isAuth, async (req, res) =>{
+    const movieId = req.params.movieId;
+    const movie = await movieService.getOne(movieId);
+
+    res.render('movies/edit', { movie, pageTitle:'Movie Magic / edit' });
+})
+
+movieController.post('/:movieId/edit', isAuth, async (req, res) =>{
+    const movieId = req.params.movieId;
+    const movieData = req.body;
+
+    await movieService.edit(movieId, movieData);
+
+    res.redirect(`/movies/${movieId}/details`);
+});
+
 
 export default movieController;
 
